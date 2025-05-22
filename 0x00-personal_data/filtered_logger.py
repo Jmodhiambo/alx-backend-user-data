2531c1e6f886b80e2d3490c52b2105d8cd2b40b0
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-22 14:47:17 +0300

    Task 0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,11 @@ The function filter_datum that returns the log message obfuscated
 import re
 from typing import List
 
-def filter_datum(
-    fields: List[str], redaction: str, message:str, separator: str
-) -> str:
-    """Returns an obfucated log message by the redaction."""
-    
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Returns the log message with sensitive fields obfuscated."""
+    return re.sub(
+        f"({'|'.join(fields)})=.+?{separator}",
+        lambda m: f"{m.group(1)}={redaction}{separator}",
+        message
+    )
