3958f2560a71a3a09e700ade9ebf431f112b55b4
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-29 19:28:55 +0300

    Task 1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Class SessionAuth that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """A class that inherits from Auth"""
+    pass
