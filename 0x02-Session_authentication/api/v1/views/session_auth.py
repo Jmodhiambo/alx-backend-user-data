4ae316556ce0826ae0742cd1cc39e0a1a138eef6
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-30 04:14:16 +0300

    Task 8. Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,3 +35,12 @@ def session_auth_login():
     session_name = getenv("SESSION_NAME")
     response.set_cookie(session_name, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """DELETE /auth_session/logout route to handle logout"""
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
