cb371de6f30a19b8a788ff967a76a96602df1852
jmodhiambo <jmodhiambo9346@gmail.com>
2025-06-05 18:13:15 +0300

    Task 2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -44,7 +46,7 @@ class DB:
         Returns the first row found in the users table as filtered by
         the method’s input arguments."""
 
-       session = self._session
+        session = self._session
 
         if not kwargs:
             raise InvalidRequestError
